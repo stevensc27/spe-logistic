61f2d34df091671391ecea0c8dc8a3368274d535
ssuarez <liderinformatica@spe.com.co>
2019-09-18 14:53:33 -0500

    Commit test 4

--- app/src/main/java/com/example/spe_logistic/MainActivity.java
+++ app/src/main/java/com/example/spe_logistic/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String clone2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
